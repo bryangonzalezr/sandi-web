a83ff0a9674e5ae986bf49aaff3c92c7b6a0cee7
Benjamin Campos <benjamin.campos@neering.cl>
2024-10-07 19:30:29 -0300

    ADD laravel echo config

---
+++ sandiapp-web/src/plugins/reverb.js
@@ -0,0 +1,17 @@
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+Pusher.logToConsole = import.meta.env.VITE_REVERB_APP_DEBUG === "true";
+
+window.Pusher = Pusher;
+
+// Crea una instancia de Echo y configura el canal y el evento de Laravel Reverb
+export const echo = new Echo({
+  broadcaster: "reverb",
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+  wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+  enabledTransports: ["ws", "wss"],
+});
